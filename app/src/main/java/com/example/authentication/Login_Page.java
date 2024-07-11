30bf9f75358b0da95eb77895a0f90fb0e289d244
Sushant Adhikari <sushantadk18@gmail.com>
2024-07-11 08:23:55 +0545

    Second Push

--- app/src/main/java/com/example/authentication/Login_Page.java
+++ app/src/main/java/com/example/authentication/Login_Page.java
@@ -20,7 +20,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login_Page extends AppCompatActivity {
-    TextView email,password,forgotPassword,signup;
+    TextView email, password, forgotPassword, signup;
     Button loginBtn;
     FirebaseAuth firebaseAuth;
 
@@ -29,50 +29,62 @@ public class Login_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
-        firebaseAuth=FirebaseAuth.getInstance();
-        email=findViewById(R.id.txt_email);
-        password=findViewById(R.id.txt_password);
-        forgotPassword=findViewById(R.id.txt_forgot);
-        loginBtn=findViewById(R.id.btn_login);
-        signup.setOnClickListener(new View.OnClickListener() {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.txt_email);
+        password = findViewById(R.id.txt_password);
+        forgotPassword = findViewById(R.id.txt_forgot);
+        signup = findViewById(R.id.txt_signup);
+        loginBtn = findViewById(R.id.btn_login);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Email=email.getText().toString().trim();
-                String Password=password.getText().toString().trim();
-                if (Email.isEmpty()){
-                    Toast.makeText(Login_Page.this,"Please enter email",Toast.LENGTH_SHORT).show();
+                String Email = email.getText().toString().trim();
+                String Password = password.getText().toString().trim();
+
+                if (Email.isEmpty()) {
+                    Toast.makeText(Login_Page.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                if (Password.isEmpty()){
-                    Toast.makeText(Login_Page.this,"Please enter Password",Toast.LENGTH_SHORT).show();
+
+                if (Password.isEmpty()) {
+                    Toast.makeText(Login_Page.this, "Please enter Password", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                if (password.length()>6){
-                    Toast.makeText(Login_Page.this,"Password too short",Toast.LENGTH_SHORT).show();
-                }
-                firebaseAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+
+
+                firebaseAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(Login_Page.this,"Signup Complete",Toast.LENGTH_SHORT).show();
-                            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                            Toast.makeText(Login_Page.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                             startActivity(intent);
                             finish();
-
-                        }
-                        else {
-                            Toast.makeText(Login_Page.this,"Signup Failed",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Login_Page.this, "Login Failed", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-
             }
         });
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(Login_Page.this, Signup_Page.class);
+                startActivity(intent);
             }
         });
 
-
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_Page.this, ForgotPasssword_Page.class);
+                startActivity(intent);
+            }
+        });
     }
 }
